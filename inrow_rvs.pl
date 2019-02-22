3ec0792abc7129c1aec4d0087d8dc965aa6f0c5b
2g4y1 <0043664@gmail.com>
2019-02-22 13:04:24 +0100

    Update inrow_rvs.pl
    fixed nagios plugin --> monitoring plugin
    removed fluid

--- inrow_rvs.pl
+++ inrow_rvs.pl
@@ -5,11 +5,11 @@
 # Scott Nolin - scott.nolin @ ssec.wisc.edu
 # 6 June 2010
 
-#monitor APC inrowrc chillers 
+#monitor APC inrowrc chillers
 
 use strict;
 use warnings;
-use Nagios::Plugin;
+use Monitoring::Plugin;
 use Net::SNMP;
 
 use vars qw($verbose $warn $critical $timeout $result);
@@ -23,22 +23,22 @@ my $warnpercent=.95; #warn at 95% critical
 #########
 #prep the plugin and arguments
 
-my $plugin = Nagios::Plugin->new(
-	usage => "Usage: %s [-H <HOST>] [-C <community>]",
-	blurb => 'This plugin checks the status of an APC inrowrc unit.',
+my $plugin = Monitoring::Plugin->new(
+        usage => "Usage: %s [-H <HOST>] [-C <community>]",
+        blurb => 'This plugin checks the status of an APC inrowrc unit.',
 );
 
 #add community string
 $plugin->add_arg(
-	spec => 'community|C=s',
-	help => "SNMP community string",
-	required => 1,
+        spec => 'community|C=s',
+        help => "SNMP community string",
+        required => 1,
 );
 
 $plugin->add_arg(
-	spec => 'host|H=s',
-	help => "hostname",
-	required => 1,
+        spec => 'host|H=s',
+        help => "hostname",
+        required => 1,
 );
 
 ## parse arguments and process standard
@@ -46,34 +46,32 @@ $plugin->getopts;
 
 #define oids
 my $oid_powernet393 = '.1.3.6.1.4.1.318.1.1.13'; #base powernet mib
-my $oid_thresh_inlet_temp = "$oid_powernet393.4.5.2.4.2.0"; 
-my $oid_thresh_supply_temp = "$oid_powernet393.4.5.2.4.4.0"; 
-my $oid_thresh_return_temp = "$oid_powernet393.4.5.2.4.6.0"; 
-my $oid_thresh_enterfluid_temp = "$oid_powernet393.5.7.0"; 
+my $oid_thresh_inlet_temp = "$oid_powernet393.4.5.2.4.2.0";
+my $oid_thresh_supply_temp = "$oid_powernet393.4.5.2.4.4.0";
+my $oid_thresh_return_temp = "$oid_powernet393.4.5.2.4.6.0";
 my $oid_inlet_temp = "$oid_powernet393.4.5.2.1.7.0";#roel aangepast
 my $oid_supply_temp = "$oid_powernet393.4.5.2.1.9.0";#roel aangepast
 my $oid_return_temp = "$oid_powernet393.4.5.2.1.11.0";#roel aangepast
-my $oid_enterfluid_temp = "$oid_powernet393.2.23.0";
 my $oid_cool_demand ="$oid_powernet393.4.5.2.1.3.0";
 my $oid_cool_output ="$oid_powernet393.4.5.2.1.2.0";
 my $oid_airflow = "$oid_powernet393.4.5.2.1.5.0";
 
 
-my @oids = ($oid_thresh_inlet_temp, $oid_thresh_supply_temp, $oid_thresh_return_temp, $oid_thresh_enterfluid_temp, $oid_inlet_temp, $oid_supply_temp, $oid_return_temp, $oid_enterfluid_temp, $oid_cool_demand, $oid_cool_output, $oid_airflow);
+my @oids = ($oid_thresh_inlet_temp, $oid_thresh_supply_temp, $oid_thresh_return_temp, $oid_inlet_temp, $oid_supply_temp, $oid_return_temp, $oid_cool_demand, $oid_cool_output, $oid_airflow);
 
 
 ########
 #get the data via snmp
 
 my ($session, $session_error) = Net::SNMP->session(
-	-hostname => $plugin->opts->host,
-	-community => $plugin->opts->community,
-	-port	=> 161,
-#	-debug => 1,
-	-version => 2,
+        -hostname => $plugin->opts->host,
+        -community => $plugin->opts->community,
+        -port   => 161,
+#       -debug => 1,
+        -version => 2,
 );
 
-if (!defined($session)) { 
+if (!defined($session)) {
         $plugin->nagios_exit(
                 return_code => 'WARNING',
                 message => "$session_error",
@@ -81,10 +79,10 @@ if (!defined($session)) {
 }
 
 my $result = $session->get_request(
-	-varbindlist	=> \@oids, 
+        -varbindlist    => \@oids,
 );
 
-if (!defined($result)) {  
+if (!defined($result)) {
         $plugin->nagios_exit(
                 return_code => 'WARNING',
                 message => "Error getting SNMP data.",
@@ -95,12 +93,12 @@ if (!defined($result)) {
 #check for missing values
 my $error=0;
 foreach my $oid(@oids) {
-	if (!defined($result->{$oid})) {
-		$error++;
-		print "ERROR: did not get $oid data";
-	}
+        if (!defined($result->{$oid})) {
+                $error++;
+                print "ERROR: did not get $oid data";
+        }
 }
-if ($error != 0) {  
+if ($error != 0) {
         $plugin->nagios_exit(
                 return_code => 'WARNING',
                 message => "Failed to get specific oid",
@@ -115,66 +113,55 @@ my $inlet = $result->{$oid_inlet_temp}/10;
 print ("Inlet:$inlet ");
 
 if ($result->{$oid_inlet_temp} > $result->{$oid_thresh_inlet_temp}){
-	print "over threshold! ";
-	$error++;
+        print "over threshold! ";
+        $error++;
 }
 elsif ($result->{$oid_inlet_temp} > $warnpercent*$result->{$oid_thresh_inlet_temp}){
-	print "approaches threshold! ";
-	$warn++;
+        print "approaches threshold! ";
+        $warn++;
 }
 
 my $supply = $result->{$oid_supply_temp}/10;
 print "Supply: $supply ";
-	
+
 if ($result->{$oid_supply_temp} > $result->{$oid_thresh_supply_temp}){
-	print "exceeds threshold! ";
-	$error++;
+        print "exceeds threshold! ";
+        $error++;
 }
 elsif ($result->{$oid_supply_temp} > $warnpercent*$result->{$oid_thresh_supply_temp}){
-	print "approaches threshold! ";
-	$warn++;
+        print "approaches threshold! ";
+        $warn++;
 }
 
 my $return = $result->{$oid_return_temp}/10;
 print "Return: $return ";
-	
+
 if ($result->{$oid_return_temp} > $result->{$oid_thresh_return_temp}){
-	print "exceeds threshold! ";
-	$error++;
+        print "exceeds threshold! ";
+        $error++;
 }
 elsif ($result->{$oid_return_temp} > $warnpercent*$result->{$oid_thresh_return_temp}){
-	print "approaches threshold! ";
-	$warn++;
+        print "approaches threshold! ";
+        $warn++;
 }
 
-my $fluid = $result->{$oid_enterfluid_temp}/10;
-print "Fluid Enter: $fluid ";
-	
-if ($result->{$oid_enterfluid_temp} > $result->{$oid_thresh_enterfluid_temp}){
-	print "exceeds threshold! ";
-	$error++;
-}
-elsif ($result->{$oid_enterfluid_temp} > $warnpercent*$result->{$oid_thresh_enterfluid_temp}){
-	print "approaches threshold! ";
-	$warn++;
-}
 
 ###add perfdata
 $plugin->add_perfdata(
-	label =>"airflow",
-	value =>$result->{$oid_airflow},
-	);
+        label =>"airflow",
+        value =>$result->{$oid_airflow},
+        );
 
 $plugin->add_perfdata(
-	label =>"cool_demand",
-	value =>$result->{$oid_cool_demand},
-	uom => "%"
-	);
+        label =>"cool_demand",
+        value =>$result->{$oid_cool_demand},
+        uom => "%"
+        );
 $plugin->add_perfdata(
-	label =>"cool_output",
-	value =>$result->{$oid_cool_output},
-	uom => "%"
-	);
+        label =>"cool_output",
+        value =>$result->{$oid_cool_output},
+        uom => "%"
+        );
 
 
 $plugin->add_perfdata(
@@ -184,24 +171,24 @@ $plugin->add_perfdata(
    );
 
 $plugin->add_perfdata(
-	label =>"supply_temp",
-	value => $supply,
+        label =>"supply_temp",
+        value => $supply,
 
 );
 
 $plugin->add_perfdata(
-	label => "return_temp",
-	value => $return,
+        label => "return_temp",
+        value => $return,
 
 );
 
-	
+
 if ($error != 0) { $plugin->nagios_exit(
-	return_code => 'CRITICAL', 
-	message => "Errors: $error\n"
-	);
+        return_code => 'CRITICAL',
+        message => "Errors: $error\n"
+        );
 }
-if ($warn != 0) {  
+if ($warn != 0) {
         $plugin->nagios_exit(
                 return_code => 'WARNING',
                 message => "Warnings: $warn",
@@ -213,4 +200,3 @@ if ($warn != 0) {
 $plugin->nagios_exit('OK',"");
 
 print "\n";
-
